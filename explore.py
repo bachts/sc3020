c6c6bcf2171625f63dcf52038d2d0c72b1bb9b94
bachts <tunfbachvn2003@gmail.com>
2023-11-13 22:32:55 +0800

    names from query

--- explore.py
+++ explore.py
@@ -26,3 +26,46 @@ def get_tables(cursor):
     result[table] = columns
     
   return result
+
+def extract_table_names(query):
+  
+    ''' Return all relations used in a SQL query statement'''
+    query = query.replace(";"," ")
+    query = query.replace(","," ")
+    import re
+    relation_names = ['nation', 'customer', 'orders', 
+                      'lineitem', 'part', 'supplier', 
+                      'region', 'partsupp']
+    print(query)
+    
+    sql_keywords = [
+    'SELECT', 'FROM', 'WHERE', 'GROUP BY', 'HAVING', 'ORDER BY', 'JOIN',
+    'INNER JOIN', 'LEFT JOIN', 'RIGHT JOIN', 'OUTER JOIN', 'ON', 'AS',
+    'DISTINCT', 'UNION', 'INSERT', 'INTO', 'VALUES', 'UPDATE', 'SET',
+    'DELETE', 'CREATE', 'TABLE', 'ALTER', 'ADD', 'DROP', 'INDEX',
+    'CONSTRAINT', 'PRIMARY KEY', 'FOREIGN KEY', 'REFERENCES', 'CASCADE',
+    'TRUNCATE', 'COMMIT', 'ROLLBACK', 'BEGIN', 'END', 'IF', 'ELSE',
+    'CASE', 'WHEN', 'THEN', 'EXISTS'
+    ]
+    pattern = re.compile("^[a-zA-Z0-9_$]+$")
+    relations = set()
+    
+    query_words = query.split()
+    FROM_flag=False
+    for i,word in enumerate(query_words):
+        if(FROM_flag or word == 'FROM'):
+            FROM_flag=True
+            if (word in relation_names):
+                if i+1<len(query_words) and bool(pattern.match(query_words[i+1]) and query_words[i+1] not in sql_keywords):
+                    relations.add(query_words[i+1])
+                else:
+                    relations.add(word)
+                    
+    print(relations)
+    return relations
+  
+def explain_analyze(query):
+  query 
+
+
+def qep_tree(query)
--- test_connection.py
+++ test_connection.py
@@ -1,5 +1,6 @@
 import psycopg2
 import explore
+import json
 
 db_name = 'TPC-H'
 user = 'postgres'
@@ -26,9 +27,22 @@ cur = conn.cursor()
 
 # rows = cur.fetchall()
 tables = explore.get_tables(cur)
+for table in tables.keys():
+  print(table)
+  for col in tables[table]:
+    print(f'\t{col[0]}')
+    
 
+
+# print(tables)
+query = input('Enter your query here: ')
+while query!='q':
+  cur.execute('explain (analyze, buffers, format json) ' + query)
+  all = cur.fetchone()
+  print(all[0][0])
+  json_string = json.dumps(all[0][0], indent=2)
+  query = input()
+  
+print(json_string)
 conn.commit()
 conn.close()
-
-print(tables)
-  
